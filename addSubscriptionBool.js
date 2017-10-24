4c99432ea5a613e41c29a0809c1f7fdbd974fb61
Paul Beaudet <inof8or@gmail.com>
2017-10-24 15:17:28 -0400

    search a year into past

--- addSubscriptionBool.js
+++ addSubscriptionBool.js
@@ -1,20 +1,38 @@
 // migration that creates a boolean feild for letting us know whether a member is on subscription or not
 var db = connect(process.env.MONGO_URI);
 
-var cursor = db.payments.find({});
-var aMonthAgo = new Date().getTime() - 21037968000; // subtract thirty one days in millis
-var membersUpdated = 0;
-while(cursor.hasNext()){
-    var payment = cursor.next();
-    if(new Date(payment.payment_date).getTime() > aMonthAgo){
-        if(payment.member_id){
-            if(payment.txn_type === 'subscr_payment'){
-                db.members.update({_id: payment.member_id.$oid}, {$set: {subscription: true}});
-            } else {
-                db.members.update({_id: payment.member_id.$oid}, {$set: {subscription: false}});
-            }
-            membersUpdated++;
-        }
-    } // skip every traction more than a month old ...
+function notIn(term, array){
+    for(var i = 0; i < array.length; i++){
+        if(term === array[i]){return false;}
+    }
+    return true;
 }
-print(membersUpdated + ' members updated');
+
+var date = new Date();
+var pointMonth = date.getMonth();
+var accountedForMembers = [];
+
+for(var months = 0; month < 12; month++){  // check a year into past
+    var lessThanMonth = date.getTime();    // time of current
+    pointMonth--;                          // point to month before
+    date.setMonth(pointMonth);             // set to month before
+    var greaterThanMonth = date.getTime(); // time of month before
+    var cursor = db.payments.find({});     // get cursor for search of payment collection
+    while(cursor.hasNext()){
+        var payment = cursor.next();
+        if(notIn(payment.member_id.$oid, accountedForMembers)){ // for members we have yet to account for
+            var dateOfPayment = new Date(payment.payment_date).getTime();
+            if( dateOfPayment > greaterThanMonth && dateOfPayment < lessThanMonth){ // for this period
+                if(payment.member_id){
+                    if(payment.txn_type === 'subscr_payment'){
+                        db.members.update({_id: payment.member_id.$oid}, {$set: {subscription: true}});
+                    } else {
+                        db.members.update({_id: payment.member_id.$oid}, {$set: {subscription: false}});
+                    }
+                    accountedForMembers.push(payment.member_id.$oid); // note we have accounted for this member
+                }
+            } // skip every traction more than a month old ...
+        }
+    }
+}
+print(accountedForMembers.length + ' members updated');
